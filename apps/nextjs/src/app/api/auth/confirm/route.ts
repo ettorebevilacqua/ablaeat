f457df71791c1dad8e128e3bb6ab3158b4014408
ettorebevilacqua <ettorebevilacqua@gmail.com>
2024-08-14 17:44:27 +0200

    clean code

--- apps/nextjs/src/app/api/auth/confirm/route.ts
+++ apps/nextjs/src/app/api/auth/confirm/route.ts
@@ -1,4 +1,4 @@
-ort { type EmailOtpType } from '@supabase/supabase-js'
+import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest, NextResponse } from 'next/server'
 
 import { createClient } from '~/utils/supabase/server'
