c6104d0f15658b2b205c3295cacb9cf66ce5e048
ettorebevilacqua <ettorebevilacqua@gmail.com>
2024-08-20 19:48:03 +0200

    auth getUser

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,15 +1,12 @@
 import NextAuth from "next-auth";
 
 import { authConfig } from "./config";
-import  signUp from "./signup";
 
 export type { Session } from "next-auth";
-
-
+import { getUser } from './serverUser';
 
 const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
-
-export { handlers, auth, signIn, signOut, signUp };
+export { getUser, handlers, auth, signIn, signOut }; 
 
 export {
   invalidateSessionToken,
