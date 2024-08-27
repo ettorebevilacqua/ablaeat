e639b1a4d2894f624ab02735e194aecb70cd1e91
ettorebevilacqua <ettorebevilacqua@gmail.com>
2024-08-27 03:57:15 +0200

    create client with supabase.auth.getUser

--- apps/nextjs/src/utils/supabase/middleware.ts
+++ apps/nextjs/src/utils/supabase/middleware.ts
@@ -1,17 +1,46 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 import { getUser } from "@acme/auth";
-
+import {createClient} from "./server"
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
+	
+
 
 // console.log('middleware xxxx yyyyyy',  supabase.auth.getUser);
   // IMPORTANT: Avoid writing any logic between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
+  
+  
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        }
+      }
+    }
+  );
+
+	const uu = await supabase.auth.getUser()
+	// console.log('middleware user xxssssx', uu.data.user.id);
 
   const user = await getUser()
   
